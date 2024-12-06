75b3d0470d3e4d3765557376987171918d4f3ac9
Middleboyno2 <loi2003zzz@gmail.com>
2024-12-07 00:01:55 +0700

    add firestore, libraries vao project, thieu storage vì no money :))

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    const MyApp()
+  );
 }
 
 class MyApp extends StatelessWidget {
